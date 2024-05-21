0e03eb8badedd644ad32c9a7c6fbf985417c1649
Sam948-byte <techguy763@gmail.com>
2024-05-21 07:58:15 -0500

    remove superfluous file

--- genRandomThreaded.py
+++ genRandomThreaded.py
@@ -1,5 +1,5 @@
 import json
-import genPassAndHashes
+import createPasswordsThreaded
 import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
@@ -10,7 +10,7 @@ def gen_randoms(num_hashes):
 
     with ThreadPoolExecutor() as executor:
         # Submit all tasks
-        futures = [executor.submit(genPassAndHashes.gen_pass_and_hashes) for _ in range(num_hashes)]
+        futures = [executor.submit(createPasswordsThreaded.create_random_password) for _ in range(num_hashes)]
         
         # Wait for tasks to complete and collect results
         for future in tqdm(as_completed(futures), total=num_hashes):
